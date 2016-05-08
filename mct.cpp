7277f74aab6625b6c1dd528ef3e663c80d460c9d
Stephen Yuan <Stephen Yuan>
2016-05-08 16:02:33 +0800

    5/85/85/85/85/8

--- mct.cpp
+++ mct.cpp
@@ -3,35 +3,44 @@
 #include <cmath>
 #include <iostream>
 using namespace std;
-Node::Node(int _M,int _N,int* _top,int** _board,int _iter):M(_M),N(_N),iter(_iter)
+
+
+Node AllNodes[20000];
+int nodeCount=0;
+Node::Node(int _M,int _N,int _top[12],int _board[12][12],int _iter):M(_M),N(_N),iter(_iter)
 {
-	board = new int*[M];
 	for (int i = 0; i < M; i++)
 	{
-		board[i] = new int[N];
 		for (int j = 0; j < N; j++)
 			board[i][j] = _board[i][j];
 	}
-	top = new int[N];
 	for(int i=0;i<N;i++)
 		top[i]=_top[i];
 	
 }
 Node::~Node()
 {
+	/*
 	delete[]childState;
 	for (int i = 0; i < childNum; i++)
 		if (child[i])
 			delete child[i];
 	delete[]child;
+	*/
+}
+void Node::initInit(int MM, int NN, int _top[12], int _board[12][12], int _iter)
+{
+	this->M = MM; this->N = NN; this->iter = _iter;
 	for (int i = 0; i < M; i++)
-		delete[]board[i];
-	delete[]board;
-	delete[]top;
+	{
+		for (int j = 0; j < N; j++)
+			board[i][j] = _board[i][j];
+	}
+	for (int i = 0; i<N; i++)
+		top[i] = _top[i];
 }
 void Node::init()
 {
-	childState=new int[12];
 	childNum=0;
 	for(int i=0;i<N;i++)
 		if(top[i]!=0)
@@ -45,28 +54,37 @@ bool Node::notTerminal(){
 	return childNum!=0;
 }
 bool Node::notOver(int x,int y,int& success){
-	//for (int i = 0; i < M; i++)
-	//{
-	//	for (int j = 0; j < N; j++)
-	//	{
-	//		cout << board[i][j] << " ";
-	//	}
-	//	cout << endl;
-	//}
-	//cout << x << " " << y << endl;
-	if(userWin(x,y,M,N,(int**)board))
+
+	if(userWin(x,y,M,N,board))
 	{
 		success=1;
 		return false;
 	}
-	if(machineWin(x,y,M,N,(int**)board))
+	if(machineWin(x,y,M,N,board))
 	{
 		success=-1;
 		return false;
 	}
-	if(isTie(N,top))
+	if (isTie(N, top))
+	{
+		success = 0;
 		return false;
+	}
 	return true;
+}
+bool MCT::over(int x, int y, int localTop[12], int localBoard[12][12] ,int& success)
+{
+	if (userWin(x, y, M, N, localBoard) || machineWin(x, y, M, N, localBoard))
+	{
+		success = 1;
+		return true;
+	}
+	if (isTie(N, localTop))
+	{
+		success=0;
+		return true;
+	}
+	return false;
 }
 bool Node::expandable()
 {
@@ -77,7 +95,7 @@ bool MCT::timeLimited()
 	finish=clock();
 	double duration=(double)(finish-start)/CLOCKS_PER_SEC;
 	//cout << duration << endl;
-	return duration>4.0;
+	return duration>2.5;
 }
 
 void MCT::uctSearch()
@@ -85,11 +103,22 @@ void MCT::uctSearch()
 	_root=new Node(M,N,top,board,0);
 	_root->init();
 	_root->p=NULL;
-	start=clock();
+	//start=clock();
+	srand(time(NULL));
 	while(!timeLimited())
 	{
-		Node* vl=treePolicy(_root);
-		float delta=defaultPolicy(vl);
+		int success=-1;
+		Node* vl=treePolicy(_root,success);
+		float delta;
+		if(success==-1)
+		{
+			if(over(vl->x,vl->y,vl->top,vl->board,success))
+				delta=success*100;
+			else
+				delta=defaultPolicy(vl);
+		}
+		else
+			delta=success*100;
 		backUp(vl,delta);
 	}
 	Node* c = bestChild(_root);
@@ -101,18 +130,22 @@ void MCT::uctSearch()
 			y = i; break;
 		}
 	}
-		
 	x = c->top[i];
-	cout << x << " " << y << endl;
+//	cout << x << " " << y << endl;
 }
-Node* MCT::treePolicy(Node* v0)
+Node* MCT::treePolicy(Node* v0,int& success)
 {
-	while(v0->notTerminal())
+	int xx=lastX,yy=lastY;
+	success=-1;
+	while(!over(xx,yy,v0->top,v0->board,success))
 	{
 		if(v0->expandable())
 			return expand(v0);
 		else
+		{
 			v0=bestChild(v0);
+			xx=v0->x;yy=v0->y;
+		}
 	}
 	return v0;
 }
@@ -125,10 +158,18 @@ Node* MCT::expand(Node* v)
 		if(!v->child[i])
 			if(j==0)
 			{
-				Node* c=(v->child[i]=new Node(M,N,(int*)v->top,(int**)v->board,v->iter+1));
+				//i = v->activateChild;
+				//Node* c=(v->child[i]=new Node(M,N,v->top,v->board,v->iter+1));
+				Node* c = (v->child[i] = &AllNodes[nodeCount++]);
+				c->initInit(M, N, v->top, v->board, v->iter + 1);
 				c->top[v->childState[i]]--;
 				//cout << c->top[v->childState[i]] << " " << v->childState[i] << endl;
-				c->board[c->top[v->childState[i]]][v->childState[i]]=(c->iter)%2+1;
+				int xx = c->top[v->childState[i]];
+				int yy = v->childState[i];
+				c->x = xx;
+				c->y = yy;
+				v->activateChild++;
+				c->board[xx][yy]=(c->iter+1)%2+1;
 				count++;
 				c->init();
 				c->p=v;
@@ -137,7 +178,7 @@ Node* MCT::expand(Node* v)
 			else
 				j--;
 	}
-	v->activateChild++;
+	
 	return v;
 }
 
@@ -159,40 +200,85 @@ Node* MCT::bestChild(Node* v)
 
 float MCT::score(Node* v)
 {
-	return float(v->winNum)/float(v->totalNum)+c*sqrt(2*log(v->p->totalNum)/float(v->totalNum));
+	return float(v->winNum)/float(v->totalNum);//+0.1*sqrt(2*log(v->p->totalNum)/float(v->totalNum));
 }
 
-float MCT::defaultPolicy(Node* &v)
+float MCT::defaultPolicy(Node*& v)
 {
-	int i=rand()%v->childNum;
-	int success=0;
-	while(true)
+	//cout << count << endl;
+	if (v->childNum == 0)
+		return 0;
+	//int k = rand() % v->childNum;
+	int success = 0;
+	float value = 0;
+	int localBoard[12][12];
+	int localTop[20];
+	for (int round = 0; round < 100; round++)
 	{
-		Node* c=(v->child[i]=new Node(M,N,(int*)v->top,(int**)v->board,v->iter+1));
-		int xxx = c->top[v->childState[i]];
-		c->top[v->childState[i]]--;
-		//cout << c->top[v->childState[i]] << " " << v->childState[i] << endl;
-		c->board[c->top[v->childState[i]]][v->childState[i]] = (c->iter) % 2 + 1;
-		c->init();
-		c->p=v;
-		count++;
-		
-		int xx = c->top[v->childState[i]];
-		int yy = v->childState[i];
-		v = c;
-		if (!v->notOver(xx,yy , success))
-			break;
-		i=rand()%v->childNum;
-		
+		for (int i = 0; i < M; i++)
+			for (int j = 0; j < N; j++)
+				localBoard[i][j] = v->board[i][j];
+		for (int j = 0; j < N; j++)
+		{
+			localTop[j] = v->top[j];
+		}
+		int iter = 0;
+		int xx=v->x, yy=v->y;
+		while (!over(xx, yy, localTop, localBoard, success))
+		{
+			iter++;
+			int k = rand() % N;
+			int i = 0;
+			while (true)
+			{
+				if (localTop[i] != 0 && k == 0)
+					break;
+				if (localTop[i] != 0) k--;
+				i = (i + 1) % N;
+			}
+			localTop[i]--;
+			xx = localTop[i]; yy = i;
+			localBoard[xx][yy] = (v->iter + iter +1) % 2 + 1;
+		}
+		if (success != 0)
+			value += 1.0 - 2.0 * float(iter % 2);
 	}
-	return success;
+	return value;
 }
+//
+//float MCT::defaultPolicy(Node* &v)
+//{
+//	if (v->childNum == 0)
+//		return 0;
+//	int i=rand()%v->childNum;
+//	int success=0;
+//	while(true)
+//	{
+//		Node* c=(v->child[i]=new Node(M,N,v->top,v->board,v->iter+1));
+//		int xxx = c->top[v->childState[i]];
+//		c->top[v->childState[i]]--;
+//		//cout << c->top[v->childState[i]] << " " << v->childState[i] << endl;
+//		c->board[c->top[v->childState[i]]][v->childState[i]] = (c->iter) % 2 + 1;
+//		c->init();
+//		c->p=v;
+//		count++;
+//		
+//		int xx = c->top[v->childState[i]];
+//		int yy = v->childState[i];
+//		v = c;
+//		if (!v->notOver(xx,yy , success))
+//			break;
+//		i=rand()%v->childNum;
+//		
+//	}
+//	return success;
+//}
 
 void MCT::backUp(Node* v,float delta)
-{
+ {
 	while(v!=NULL)
 	{
-		v->totalNum++;
+		v->totalNum+=100;
 		v->winNum+=delta;
 		delta=-delta;
 		v=v->p;
--- mct.h
+++ mct.h
@@ -6,21 +6,24 @@
 using namespace std;
 class Node{
 public:
-	Node(int _M,int _N,int* top,int** _board,int _iter);
+	Node() {}
+	Node(int _M,int _N,int top[12],int _board[12][12],int _iter);
 	~Node();
+	void initInit(int _M, int _N, int top[12], int _board[12][12], int _iter);
 	void init();
-	int **board;
-	int *top;
+	int board[12][12];
+	int top[12];
 	int M;
 	int N;
 	int iter;
-	
+	int x;
+	int y;
 	float winNum;
 	int totalNum;
 	float score;
 	Node* p;
 	Node** child;
-	int* childState;
+	int childState[12];
 	int childNum;
 	int activateChild;
 	bool notTerminal();
@@ -39,27 +42,27 @@ public:
 	int N;
 	clock_t start;
 	clock_t finish;
-	int* top;
-	int** board;
+	int *top;
+	int board[12][12];
 	Node* _root;
 	float c;
 	bool timeLimited();
 	float score(Node* v);
-	MCT(int _x,int _y,int _lastX, int _lastY, int _M, int _N, int* _top, int** _board):x(_x),y(_y),lastX(_lastX),lastY(_lastY),M(_M),N(_N),top(_top)
+	MCT(int _x,int _y,int _lastX, int _lastY, int _M, int _N, int* _top, int** _board, clock_t _s):x(_x),y(_y),lastX(_lastX),lastY(_lastY),M(_M),N(_N),top(_top),start(_s)
 	{
 		count = 0;
-		board = new int*[M];
+		//board = new int*[M];
 		for (int i = 0; i < M; i++)
 		{
-			board[i] = new int[N];
+			//board[i] = new int[N];
 			for (int j = 0; j < N; j++)
 				board[i][j] = _board[i][j];
 		}
 	}
 	~MCT(){}
-	
+	bool over(int, int, int lt[12], int lb[12][12], int&);
 	void uctSearch();
-	Node* treePolicy(Node*);
+	Node* treePolicy(Node*,int&);
 	Node* expand(Node* );
 	Node* bestChild(Node*);
 	float defaultPolicy(Node* &);
